0e03a4f5a19c531fa02302e5eed7bb90a89ec90a
ANDREW PLANTE <plante@nike.cs.uga.edu>
2019-12-10 16:38:34 -0500

    Started on Reversi

--- src/main/java/cs1302/arcade/Sprite.java
+++ src/main/java/cs1302/arcade/Sprite.java
@@ -70,5 +70,9 @@ public class Sprite
     {
         return s.getBoundary().intersects( this.getBoundary() );
     }
+    public boolean intersects(double x, double y) {
+        Rectangle2D temp = new Rectangle2D(x,y,1,1);
+        return this.getBoundary().intersects(temp);
+    }
 
 }
